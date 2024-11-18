738680606f85b07dd19414d7d3a45f52f2e26dcb
Obsessive-Coder <jaredhuff85@gmail.com>
2024-11-17 22:09:40 -0700

    duh

--- src/App.js
+++ src/App.js
@@ -78,9 +78,6 @@ function App() {
             <CardTitle>
               Special Title Treatment
             </CardTitle>
-            {/* <CardText>
-              With supporting text below as a natural lead-in to additional content.
-            </CardText> */}
 
             <BetaCountdown />
           </CardBody>
