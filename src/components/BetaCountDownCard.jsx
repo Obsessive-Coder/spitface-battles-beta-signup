09ed3ea4c76d3c6937070ad5b7d4379d0025e3e1
Jared Huffstutler <13920464+Obsessive-Coder@users.noreply.github.com>
2025-01-28 03:33:11 -0700

    finalizing countdown card

--- src/components/BetaCountDownCard.jsx
+++ src/components/BetaCountDownCard.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
+// Font Awesome icons.
+import { faHandHoldingUsd, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 // Bootstrap Components.
 import { Button, Card, CardBody, CardHeader, CardFooter, CardTitle, CardText } from 'reactstrap';
 
@@ -10,15 +14,30 @@ function BetaCountDownCard() {
 
   return (
     <Card className="rounded-0 border-0 bg-darkest text-secondary main-card">
-      <CardHeader
-        className="bg-darkest rounded-0 border-primary-orange"
-        style={{ lineHeight: '33px' }}
-      >
-        <CardTitle className="text-center fs-1 fw-bold text-uppercase m-0">
-          SPITFACE
-          <br />
-          BATTLES
-        </CardTitle>
+      <CardHeader className="bg-darkest rounded-0 border-primary-orange d-flex justify-content-between align-items-center">
+        <div className='flex-grow-1 flex-basis-0' style={{lineHeight: '17px'}}>
+          <p className='text-center m-0 fw-bold fs-4'>Beta<br/>Launch</p>
+        </div>
+
+        <div className='flex-fill'>
+          <CardTitle className="text-center fs-1 fw-bold text-uppercase m-0" style={{ lineHeight: '33px' }}>
+            SPITFACE
+            <br />
+            BATTLES
+          </CardTitle>
+        </div>
+
+        <div className='d-flex flex-column justify-content-between align-self-stretch flex-grow-1 flex-basis-0 text-end'>
+          <div className='d-flex justify-content-end'>
+            <FontAwesomeIcon icon={faHandHoldingUsd} size="lg" className='mx-2' />
+            <small className='fw-bold'>8888</small>
+          </div>
+
+          <div className='d-flex justify-content-end'>
+            <FontAwesomeIcon icon={faUsers} size="lg" className='mx-2' />
+            <small className='fw-bold'>8888</small>
+          </div>
+        </div>
       </CardHeader>
 
       <CardBody className="text-bg-darkest rounded-0 py-0">
@@ -37,8 +56,12 @@ function BetaCountDownCard() {
         />
       </CardBody>
 
-      <CardFooter className="text-bg-darkest border-0">
-        Footer
+      <CardFooter className="d-flex text-bg-darkest border-0">
+        <div className='flex-grow-1 flex-basis-0 text-center border pb-1'>Your Stage</div>
+
+        <div className='flex-grow-1 flex-basis-0 text-center border pb-1 mx-2'>Your Skills</div>
+
+        <div className='flex-grow-1 flex-basis-0 text-center border pb-1'>Your Voice</div>
       </CardFooter>
     </Card>
   )
