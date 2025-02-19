7509dffa8b4bb965439fe8f2a9527f5bdf5f82d2
Obsessive-Coder <jaredhuff85@gmail.com>
2025-02-19 14:42:07 -0700

    cleanup for firebase

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,9 @@ import { validateEmail, validateUsername } from './validation';
 
 export { validateUsername };
 export { validateEmail };
+
+export const validation = { validateEmail, validateUsername };
+
+const defaultExport = { validation };
+
+export default defaultExport;
